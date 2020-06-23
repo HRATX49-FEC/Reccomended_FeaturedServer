d95b48debd114d543e3e4ee51890a8f982eb309c
Anthony Casper <tonycasper21@gmail.com>
2020-06-23 11:11:54 -0500

    Initial deployment using EB - no DB

---
+++ index.compiled.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const path = require('path');
+
+const db = require('./db/querys.js');
+
+const PORT = 5001;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.get('/reccomended', (req, res) => {
+  db.getCat(req.query.catName).then(results => {
+    //restructure the results to send back to the client
+    let cat = results[0];
+    cat.url = [cat.url];
+
+    for (let i = 1; i < results.length; i++) {
+      cat.url.push(results[i].url);
+    } //send the restrucured results to the client
+
+
+    res.status(200).send(cat);
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(404);
+  });
+});
+app.get('/reccomended/all', (req, res) => {
+  db.getCats().then(results => {
+    //restructure the results to send back to the client
+    // let cat = results[0];
+    // cat.url = [cat.url]
+    // for(let i = 1; i < results.length; i++) {
+    //   cat.url.push(results[i].url)
+    // }
+    //send the restrucured results to the client
+    res.status(200).send(results);
+  }).catch(err => {
+    console.log(err);
+    res.sendStatus(404);
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Express is listening on port ${PORT}.`);
+});
